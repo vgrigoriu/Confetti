97168eed82c301f0efe5fbf60e6121fa9aec561f
Victor Grigoriu <vgrigoriu@gmail.com>
2015-12-31 16:23:55 +0200

    First version where tests run

---
+++ src/ConfettiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConfettiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConfettiTests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("020afb55-5c6d-49e0-90f0-7cf50dc8869a")]
---
+++ src/ConfettiTests/SanityTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ConfettiTests
+{
+    public class SanityTest
+    {
+        [Fact]
+        public void CanRunTests()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
