37110bd6e9161b530c3ff4dca2e976e430781853
Victor Grigoriu <vgrigoriu@gmail.com>
2015-12-31 17:59:45 +0200

    Add exception documentation.

--- src/Confetti/Settings.cs
+++ src/Confetti/Settings.cs
@@ -17,6 +17,8 @@
         /// <typeparam name="T">The type of the value to return</typeparam>
         /// <param name="key">The name of the setting key</param>
         /// <returns>The setting named `key`, parsed as a value of type `T`</returns>
+        /// <exception cref="MissingKeyException">The key was not found.</exception>
+        /// <exception cref="MalformedValueException">The value could not be parsed.</exception>
         public T GetSetting<T>(string key)
         {
             string rawValue;
