81f5b644cfdacf48c3e706cc9c10a70c5bb9d44a
Victor Grigoriu <vgrigoriu@gmail.com>
2016-01-01 16:54:54 +0200

    Add MultipleSettingsSource

---
+++ src/Confetti/MultipleSettingsSource.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Confetti
+{
+    public class MultipleSettingsSource : IRawSettingsSource
+    {
+        private readonly IReadOnlyCollection<IRawSettingsSource> settingsSources;
+
+        public MultipleSettingsSource(IReadOnlyCollection<IRawSettingsSource> settingsSources)
+        {
+            this.settingsSources = settingsSources;
+        }
+
+        public bool TryGetRawSetting(string key, out string value)
+        {
+            foreach (var settingsSource in settingsSources)
+            {
+                if (settingsSource.TryGetRawSetting(key, out value))
+                {
+                    // found it!
+                    return true;
+                }
+            }
+
+            // no luck
+            value = null;
+            return false;
+        }
+    }
+}
---
+++ src/ConfettiTests/MultipleSettingsSourceTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Confetti;
+using Xunit;
+
+namespace ConfettiTests
+{
+    public class MultipleSettingsSourceTests
+    {
+        private readonly MultipleSettingsSource sut;
+
+        public MultipleSettingsSourceTests()
+        {
+            Environment.SetEnvironmentVariable("EnvironmentKey", "Environment value");
+            Environment.SetEnvironmentVariable("CommonKey", "Environment value for common key");
+            var environmentSettingsSource = new EnvironmentSettingsSource();
+
+            var inMemorySettingsSource = new InMemorySettingsSource(
+                new Dictionary<string, string>
+                {
+                    {"InMemoryKey", "In memory value"},
+                    {"CommonKey", "In memory value for common key" }
+                });
+
+            sut = new MultipleSettingsSource(new List<IRawSettingsSource>
+            {
+                environmentSettingsSource,
+                inMemorySettingsSource
+            });
+        }
+
+        [Fact]
+        public void CanGetSettingFromFirstSource()
+        {
+            string value;
+            var found = sut.TryGetRawSetting("EnvironmentKey", out value);
+
+            Assert.True(found, "Could not find setting in first source");
+            Assert.Equal("Environment value", value);
+        }
+
+        [Fact]
+        public void CanGetSettingFromSecondSource()
+        {
+            string value;
+            var found = sut.TryGetRawSetting("InMemoryKey", out value);
+
+            Assert.True(found, "Could not find setting in second source");
+            Assert.Equal("In memory value", value);
+        }
+
+        [Fact]
+        public void CanGetNonExistingSetting()
+        {
+            string value;
+            var found = sut.TryGetRawSetting("NoSuchKey", out value);
+
+            Assert.False(found, "NoSuchKey was found");
+        }
+
+        [Fact]
+        public void FirstSourceHasPrecedence()
+        {
+            string value;
+            var found = sut.TryGetRawSetting("CommonKey", out value);
+
+            Assert.True(found, "Could not find common key");
+            Assert.Equal("Environment value for common key", value);
+        }
+    }
+}
