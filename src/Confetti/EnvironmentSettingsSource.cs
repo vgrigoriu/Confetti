138bc5c246b3002f371c873d6fb31226e307f401
Victor Grigoriu <vgrigoriu@gmail.com>
2016-01-01 16:19:54 +0200

    Add EnvironmentSettingsSource

---
+++ src/Confetti/EnvironmentSettingsSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Confetti
+{
+    public class EnvironmentSettingsSource : IRawSettingsSource
+    {
+        public bool TryGetRawSetting(string key, out string value)
+        {
+            value = Environment.GetEnvironmentVariable(key);
+            return value != null;
+        }
+    }
+}
---
+++ src/ConfettiTests/EnvironmentSettingsSourceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Confetti;
+using Xunit;
+
+namespace ConfettiTests
+{
+    public class EnvironmentSettingsSourceTests
+    {
+        [Fact]
+        public void GetsExistingEnvironmentVariable()
+        {
+            var key = Guid.NewGuid().ToString();
+            Environment.SetEnvironmentVariable(key, "this is a test");
+
+            var sut = new EnvironmentSettingsSource();
+            string value;
+            var found = sut.TryGetRawSetting(key, out value);
+
+            Assert.True(found, "Randomly generated key was not found");
+            Assert.Equal("this is a test", value);
+        }
+
+        [Fact]
+        public void DoesntGetNonExistingEnvironmentVariable()
+        {
+            var key = Guid.NewGuid().ToString();
+
+            var sut = new EnvironmentSettingsSource();
+            string value;
+            var found = sut.TryGetRawSetting(key, out value);
+
+            Assert.False(found, "Strangely, a randomly generated key was found");
+        }
+    }
+}
