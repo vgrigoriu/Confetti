9bd80a478ef5af2ae11c8b3a15fff6c43c082cd9
Victor Grigoriu <vgrigoriu@gmail.com>
2016-01-01 16:36:30 +0200

    Add InMemorySettingsSource

---
+++ src/Confetti/InMemorySettingsSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Confetti
+{
+    public class InMemorySettingsSource : IRawSettingsSource
+    {
+        private readonly IReadOnlyDictionary<string, string> settings;
+
+        public InMemorySettingsSource(IReadOnlyDictionary<string, string> settings)
+        {
+            this.settings = settings;
+        }
+
+        public bool TryGetRawSetting(string key, out string value)
+        {
+            return settings.TryGetValue(key, out value);
+        }
+    }
+}
---
+++ src/ConfettiTests/InMemorySettingsSourceTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Confetti;
+using Xunit;
+
+namespace ConfettiTests
+{
+    public class InMemorySettingsSourceTests
+    {
+        private readonly IReadOnlyDictionary<string, string> settings = new Dictionary<string, string>
+        {
+            {"SomeKey", "Some value"}
+        };
+
+        [Fact]
+        public void GetsExistingEnvironmentVariable()
+        {
+            var sut = new InMemorySettingsSource(settings);
+            string value;
+            var found = sut.TryGetRawSetting("SomeKey", out value);
+
+            Assert.True(found, "SomeKey was not found");
+            Assert.Equal("Some value", value);
+        }
+
+        [Fact]
+        public void DoesntGetNonExistingEnvironmentVariable()
+        {
+            var sut = new InMemorySettingsSource(settings);
+            string value;
+            var found = sut.TryGetRawSetting("ThisKeyDoesNotExist", out value);
+
+            Assert.False(found, "Strangely, a non existing key was found");
+        }
+    }
+}
