6e27e3bd8c63903889d1e57278a9d53377b59de5
Victor Grigoriu <vgrigoriu@gmail.com>
2016-01-01 16:00:59 +0200

    Add DefaultSettingParser
    which uses TypeConverter to do the conversion from string

---
+++ src/Confetti/DefaultSettingParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Confetti
+{
+    public class DefaultSettingParser : IRawSettingParser
+    {
+        public Result<T> Parse<T>(string rawSettingValue)
+        {
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof (T));
+                var parsedValue = (T) converter.ConvertFromInvariantString(rawSettingValue);
+                return Result.FromValue(parsedValue);
+            }
+            catch (Exception)
+            {
+                return Result<T>.Failure;
+            }
+        }
+    }
+}
---
+++ src/ConfettiTests/DefaultSettingsParserTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using Confetti;
+using Xunit;
+
+namespace ConfettiTests
+{
+    public class DefaultSettingsParserTests
+    {
+        [Fact]
+        public void CanParseStrings()
+        {
+            var sut = new DefaultSettingParser();
+            var result = sut.Parse<string>("some string");
+
+            Assert.False(result.IsFailure);
+            Assert.Equal("some string", result.Value);
+        }
+
+        [Fact]
+        public void CanParseInts()
+        {
+            var sut = new DefaultSettingParser();
+            var result = sut.Parse<int>("112233");
+
+            Assert.False(result.IsFailure);
+            Assert.Equal(112233, result.Value);
+        }
+
+        [Fact]
+        public void CanParseDoubles()
+        {
+            var sut = new DefaultSettingParser();
+            var result = sut.Parse<double>("12.5");
+
+            Assert.False(result.IsFailure);
+            Assert.Equal(12.5, result.Value);
+        }
+
+        [Fact]
+        public void CanParseDoublesInNonEnUsLocale()
+        {
+            var oldCulture = CultureInfo.DefaultThreadCurrentCulture;
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("ro");
+
+            var sut = new DefaultSettingParser();
+            var result = sut.Parse<double>("12.5");
+
+            Assert.False(result.IsFailure);
+            Assert.Equal(12.5, result.Value);
+
+            CultureInfo.DefaultThreadCurrentCulture = oldCulture;
+        }
+
+        [Fact]
+        public void ParsingRandomStringReturnsFailure()
+        {
+            var sut = new DefaultSettingParser();
+            var result = sut.Parse<double>("this is not a double");
+
+            Assert.True(result.IsFailure);
+        }
+    }
+}
